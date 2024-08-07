91e7e9d6cf02ec99f4818b8270cf927b95037c87
ikarus <ikarus-stv@rambler.ru>
2024-08-08 00:12:59 +0300

    First commit to controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    private Long id;                // целочисленный идентификатор
+    private String name;            // название
+    private String description;     // описание
+    private LocalDate releaseDate;  // дата релиза
+    private Duration duration;      // продолжительность фильма
 }
